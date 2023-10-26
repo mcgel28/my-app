7b5831db6a2727bf801d008aee574b17e8fbef54
Your Name <you@example.com>
2023-10-26 23:26:48 +0800

    Updates

---
+++ precache-manifest.57ac4455249b8c5e7aae7af2958003c7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e68d667fc93db2444bbe7b00fa64cf",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "8b00afe43a72d4c6e27d",
+    "url": "/my-app/static/css/main.0d914919.chunk.css"
+  },
+  {
+    "revision": "41c208f0160196561246",
+    "url": "/my-app/static/js/2.638dcf89.chunk.js"
+  },
+  {
+    "revision": "b114cc85da504a772f040e3f40f8e46a",
+    "url": "/my-app/static/js/2.638dcf89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b00afe43a72d4c6e27d",
+    "url": "/my-app/static/js/main.8e78eef9.chunk.js"
+  },
+  {
+    "revision": "9fb6fedb38d1680e6ab4",
+    "url": "/my-app/static/js/runtime-main.38642c62.js"
+  },
+  {
+    "revision": "2a8917947e1ac6d23ee37a80b596bb1b",
+    "url": "/my-app/static/media/backgroundImage.2a891794.jpg"
+  },
+  {
+    "revision": "b6d79baaf7f5f29b94fc0c9809cc4147",
+    "url": "/my-app/static/media/geometricBackground.b6d79baa.png"
+  },
+  {
+    "revision": "38cfd2ae82b3c7da1d3fc52726b77125",
+    "url": "/my-app/static/media/handcoding.38cfd2ae.png"
+  },
+  {
+    "revision": "d52bc0b6aceef3b506e0500f49a14406",
+    "url": "/my-app/static/media/logo.d52bc0b6.png"
+  },
+  {
+    "revision": "d28f55e4e4942ab3fac55dedd1fbd1bd",
+    "url": "/my-app/static/media/me.d28f55e4.jpg"
+  },
+  {
+    "revision": "afcc012f316f12a1bbb2e15ea5c65cf7",
+    "url": "/my-app/static/media/me2.afcc012f.jpg"
+  },
+  {
+    "revision": "b6a6ab53bb1b0b8f5d784c75ada0fb15",
+    "url": "/my-app/static/media/react.b6a6ab53.png"
+  },
+  {
+    "revision": "feb233cad873aa7b14c5002b786e7a22",
+    "url": "/my-app/static/media/rentmoto2.feb233ca.jpg"
+  },
+  {
+    "revision": "441d381882a4a7cbb013245b707a5f1b",
+    "url": "/my-app/static/media/whiteBackground.441d3818.jpg"
+  },
+  {
+    "revision": "5c88f5e6cbb131df670e64ede1acdafb",
+    "url": "/my-app/static/media/whiteGeometricBg.5c88f5e6.jpg"
+  },
+  {
+    "revision": "276a077d752c2aedd776b3b485b33913",
+    "url": "/my-app/static/media/wvdigital.276a077d.jpg"
+  }
+]);
