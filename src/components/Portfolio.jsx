aab0ff9a879d019856dcbd10516a74b0143550d9
Your Name <you@example.com>
2023-11-06 19:10:34 +0800

    update on portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,13 +10,9 @@ function Portfolio() {
             <div className="portfolio">
                <h1 className="port-title">Portfolio</h1>
                 <div className="port-row-1">
-                    <div className="portoflio-col-1" >
+                    <div className="portoflio-col-1">
                         <a href="https://mcgel28.github.io/rent-moto/">
-<<<<<<< HEAD
-                        <img className="portfolio-img" src={rentmoto2} alt="rent motorcycle" />
-=======
-                        <img className="portfolio-img" href="https://mcgel28.github.io/rent-moto/" src={rentmoto2} alt="rent motorcycle" />
->>>>>>> 1a105e11a924157702d66b23147aa4f2b012590f
+                        <img className="portfolio-img"  src={rentmoto2} alt="rent motorcycle" />
                         </a>
                     </div>
                     <div className="portoflio-col-2" >
